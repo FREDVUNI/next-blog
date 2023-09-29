45703f06c6c395feb28eabd952a9e3f08ce9b83f
Fred Vuni <fredvuni809@gmail.com>
2023-09-29 14:03:07 +0300

    chore: create responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">{children}</div>
+        <div className="container">
+          <div className="wrapper">{children}</div>
+        </div>
       </body>
     </html>
   );
