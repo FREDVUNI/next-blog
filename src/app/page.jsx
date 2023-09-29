dc018751c7f58d1590bdd2dbace7f6719130a0ad
Fred Vuni <fredvuni809@gmail.com>
2023-09-29 14:01:55 +0300

    chore: create responsiveness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href={"/"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt hic dolor
+        nam temporibus voluptatem, culpa dolorum consequuntur magnam delectus
+        impedit modi voluptatibus, qui sed necessitatibus. Vel tempora
+        recusandae dicta tempore.
+      </Link>
+    </div>
+  );
 }
