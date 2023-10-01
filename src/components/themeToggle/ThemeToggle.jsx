db7b8e8c2b86828e10a86d39acad1ba7f0102260
Fred Vuni <fredvuni809@gmail.com>
2023-10-01 13:52:57 +0300

    chore: set up all other components

---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./theme.toggle.module.css";
+
+const ThemeToggle = () => {
+  return <div className={styles.container}>ThemeToggle</div>;
+};
+
+export default ThemeToggle;
