99e6a02aa942f4edd1bd68c5806d33f9e4f35bfc
Fred Vuni <fredvuni809@gmail.com>
2023-10-01 13:38:54 +0300

    chore: set up all other components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
+import Categories from "@/components/categories/Categories";
+import Cards from "@/components/cards/Cards";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
-    <div>
-      <Link href={"/"}>
-        <Featured/>
-      </Link>
+    <div className={styles.container}>
+      <Featured />
+      <Categories />
+      <div className={styles.content}>
+        <Cards />
+        <Menu />
+      </div>
     </div>
   );
 }
