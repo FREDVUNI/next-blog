0bbece59f1a8d9292cb7e5b8fbe3211fc752f6a0
Fred Vuni <fredvuni809@gmail.com>
2023-10-01 13:39:27 +0300

    chore: set up all other components

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./card.module.css";
+import Pagination from "../pagination/Pagination";
 
 const Cards = () => {
-  return <div className={styles.container}>cards</div>;
+  return (
+    <div className={styles.container}>
+      <Pagination />
+    </div>
+  );
 };
 
 export default Cards;
