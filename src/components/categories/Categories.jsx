f881ac1dc5e914a46629a551289471290aa73174
Fred Vuni <fredvuni809@gmail.com>
2023-10-01 13:34:16 +0300

    chore: set up all other components

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./card.module.css";
+
+const Categories = () => {
+  return <div className={styles.container}>Categories</div>;
+};
+
+export default Categories;
