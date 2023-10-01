6017c4cd2a54d6bf0e66ed43a231ddb6931cce07
Fred Vuni <fredvuni809@gmail.com>
2023-10-01 14:04:41 +0300

    chore: set up all other components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image src="./facebook.png" alt="facebook" width={24} height={24} />
-        <Image src="./instagram.png" alt="instagram" width={24} height={24} />
-        <Image src="./tiktok.png" alt="tiktok" width={24} height={24} />
-        <Image src="./youtube.png" alt="youtube" width={24} height={24} />
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
       <div className={styles.logo}>next blog</div>
       <div className={styles.links}>
