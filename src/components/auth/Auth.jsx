12632317d77fe5430f2766d57448c328dec97407
Fred Vuni <fredvuni809@gmail.com>
2023-10-02 07:41:35 +0300

    chore: set up temporary auth - hardcoded

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -4,11 +4,18 @@ import Link from "next/link";
 
 const Auth = () => {
   const auth = "not-authenticated";
-  return <div className={styles.container}>
-    {
-      auth === "not-authenticated" ? <Link href={"/"}>Login</Link>:
-    }
-  </div>;
+  return (
+    <div className={styles.container}>
+      {auth === "not-authenticated" ? (
+        <Link href={"/"}>Login</Link>
+      ) : (
+        <>
+          <Link href={"/create-post"}>create post</Link>
+          <span className={styles.Link}>Logout</span>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Auth;
