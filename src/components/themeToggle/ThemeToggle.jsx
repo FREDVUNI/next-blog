3d76d87bf1a269dd5bd4aeef70a75364fcdeaae2
Fred Vuni <fredvuni809@gmail.com>
2023-10-02 07:58:00 +0300

    chore: set up dark/light theme

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -5,8 +5,20 @@ import Image from "next/image";
 const ThemeToggle = () => {
   return (
     <div className={styles.container}>
-      <Image src="/moon.png" alt="dark" width={24} height={14} />
-      <Image src="/sun.png" alt="light" width={24} height={14} />
+      <Image
+        src="/moon.png"
+        alt="dark"
+        width={24}
+        height={14}
+        className={styles.moon}
+      />
+      <Image
+        src="/sun.png"
+        alt="light"
+        width={24}
+        height={14}
+        className={styles.sun}
+      />
     </div>
   );
 };
