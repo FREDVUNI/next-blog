52f1cb678c2be6fdddf0d90ec01548db345ce5ee
Fred Vuni <fredvuni809@gmail.com>
2023-10-14 17:06:00 +0300

    feat: work on burger menu

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 const Auth = () => {
   const auth = "not-authenticated";
+  const open = "";
   return (
     <>
       {auth === "not-authenticated" ? (
@@ -11,8 +12,28 @@ const Auth = () => {
       ) : (
         <>
           <Link href={"/create-post"}>Post</Link>
-          <span className={styles.Link}>Logout</span>
+          <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger}>
+        <div className={styles.lines}></div>
+        <div className={styles.lines}></div>
+        <div className={styles.lines}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+          {auth === "not-authenticated" ? (
+            <Link href={"/"}>Login</Link>
+          ) : (
+            <>
+              <Link href={"/create-post"}>Post</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
