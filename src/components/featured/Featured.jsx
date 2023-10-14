532df7e483a011553a3adf4f1bdffbe253de4018
Fred Vuni <fredvuni809@gmail.com>
2023-10-14 17:24:59 +0300

    feat: create the featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,14 +6,17 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey Lama dev here!</b> Discover my stories and creative ideas.
+        <strong>Hey Lama dev here!</strong> Discover my stories and creative
+        ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
           <p className={styles.postDesc}>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
             aperiam asperiores sed delectus voluptatem enim deleniti reiciendis,
