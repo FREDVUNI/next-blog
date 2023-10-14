916417b722c4c9aac069d9e822f4121458c5f92e
Fred Vuni <fredvuni809@gmail.com>
2023-10-14 17:52:51 +0300

    feat: create the popular categories section

--- src/components/categories/data.js
+++ src/components/categories/data.js
@@ -6,16 +6,26 @@ export const categories = [
   },
   {
     id: 2,
+    category: "fashion",
+    image: "/fashion.png",
+  },
+  {
+    id: 3,
+    category: "food",
+    image: "/food.png",
+  },
+  {
+    id: 4,
     category: "travel",
     image: "/travel.png",
   },
   {
-    id: 3,
+    id: 5,
     category: "culture",
     image: "/culture.png",
   },
   {
-    id: 4,
+    id: 6,
     category: "coding",
     image: "/coding.png",
   },
