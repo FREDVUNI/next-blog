f22d1a5b75ac5971a93f4b46ae26d4df9cdc9c7e
Fred Vuni <fredvuni809@gmail.com>
2023-10-15 12:42:12 +0300

    feat: create the recent posts section

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="post" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.details}>
+          <span className={styles.date}>11.05.20</span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href={"/"}>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
+          blanditiis perspiciatis, doloremque eos voluptas consectetur laborum
+          assumenda dicta vel suscipit quidem, ad quam id mollitia, possimus
+          amet iusto sed voluptate.
+        </p>
+        <Link href={"/"} className={styles.link}>
+          Read more
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./cards.module.css";
 import Pagination from "../pagination/Pagination";
+import Card from "../card/Card";
 
 const Cards = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagination />
     </div>
   );
