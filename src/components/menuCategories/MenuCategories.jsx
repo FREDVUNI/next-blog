242d59b1b0095564509e4c071fa8375c925bd525
Fred Vuni <fredvuni809@gmail.com>
2023-10-16 08:50:12 +0300

    chore: remove extra curly bracket

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -7,37 +7,37 @@ const MenuCategories = () => {
     <div className={styles.categoryList}>
       <Link
         href={"/blog?category=style"}
-        className={`${styles.categoryItem} ${styles.style}}`}
+        className={`${styles.categoryItem} ${styles.style}`}
       >
         Style
       </Link>
       <Link
         href={"/blog?category=food"}
-        className={`${styles.categoryItem} ${styles.food}}`}
+        className={`${styles.categoryItem} ${styles.food}`}
       >
         Food
       </Link>
       <Link
         href={"/blog?category=culture"}
-        className={`${styles.categoryItem} ${styles.culture}}`}
+        className={`${styles.categoryItem} ${styles.culture}`}
       >
         Culture
       </Link>
       <Link
         href={"/blog?category=coding"}
-        className={`${styles.categoryItem} ${styles.coding}}`}
+        className={`${styles.categoryItem} ${styles.coding}`}
       >
         Coding
       </Link>
       <Link
         href={"/blog?category=travel"}
-        className={`${styles.categoryItem} ${styles.travel}}`}
+        className={`${styles.categoryItem} ${styles.travel}`}
       >
         Travel
       </Link>
       <Link
         href={"/blog?category=fashion"}
-        className={`${styles.categoryItem} ${styles.fashion}}`}
+        className={`${styles.categoryItem} ${styles.fashion}`}
       >
         Fashion
       </Link>
