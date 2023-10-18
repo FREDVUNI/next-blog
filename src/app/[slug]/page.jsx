4cdb35d857256c7e1ce43017c7ffdd2af3bc2bf5
Fred Vuni <fredvuni809@gmail.com>
2023-10-18 08:22:53 +0300

    feat: create the single post page

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./singlePage.module.css";
+import Menu from "@/components/menu/Menu";
+import Image from "next/image";
+
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>10.10.2023</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="single" fill className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}></div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
