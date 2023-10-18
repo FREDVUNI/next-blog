102d879a562e99ee3c20d802a7e6b5650174fc52
Fred Vuni <fredvuni809@gmail.com>
2023-10-18 09:41:48 +0300

    fix: escape single quotes on Menu component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,13 +8,13 @@ import MenuCategories from "../menuCategories/MenuCategories";
 const Menu = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>{"whats popping?"}</h2>
+      <h2 className={styles.subtitle}>{"What's popping?"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <MenuPosts withImage={false} />
       <h2 className={styles.subtitle}>{"Discover by topic"}</h2>
       <h1 className={styles.title}>Categories</h1>
       <MenuCategories />
-      <h2 className={styles.subtitle}>{"choosen by editor"}</h2>
+      <h2 className={styles.subtitle}>{"Chosen by editor"}</h2>
       <h1 className={styles.title}>Editor's Pick</h1>
       {<MenuPosts withImage={true} />}
     </div>
