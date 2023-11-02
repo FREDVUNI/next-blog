48bf142af1cd779c2bdd099059311374612a2cf6
Fred Vuni <fredvuni809@gmail.com>
2023-11-02 18:08:29 +0300

    chore: remove the heading from feature section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 const Featured = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
+      {/* <h1 className={styles.title}>
         <strong>Hey Lama dev here!</strong> Discover my stories and creative
         ideas.
-      </h1>
+      </h1> */}
       <div className={styles.post}>
         <div className={styles.imgContainer}>
           <Image src="/p1.jpeg" alt="" fill className={styles.image} />
