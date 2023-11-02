3ab4f1549e520813ca07109350200bd201ba1855
Fred Vuni <fredvuni809@gmail.com>
2023-11-02 18:38:01 +0300

    chore: adjust the footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="lama blog" width={50} height={50} />
-          <h1 className={styles.logoText}>nextBlog</h1>
+          {/* <Image src="/logo.png" alt="lama blog" width={50} height={50} /> */}
+          <h1 className={styles.logoText}>next blog</h1>
         </div>
         <p className={styles.desc}>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
