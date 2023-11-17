ab75442559203c124ed38f3aaca594b5cb7d9f2d
Fred Vuni <fredvuni809@gmail.com>
2023-11-17 06:30:12 +0300

    feat: login with gmail

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,12 +2,17 @@
 
 import React from "react";
 import styles from "./loginPage.module.css";
+import { signIn, useSession } from "next-auth/react";
 
 const Loginpage = () => {
+  const { data, status } = useSession();
+
+  console.log(data);
+  console.log(status);
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>Sign in with Google</div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
