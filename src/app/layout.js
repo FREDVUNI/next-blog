aa2ebbfecd866b2bc05dbabea9a3a84bf6d96ec3
Fred Vuni <fredvuni809@gmail.com>
2023-11-17 16:00:27 +0300

    chore: test vercel deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${poppins.className}`}>
         <AuthProvider>
           <ThemeContextProvider>
-            <ThemeProvider>
+            <ThemeProvider> 
               <div className="container">
                 <div className="wrapper">
                   <Navbar />
