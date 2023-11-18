b45353106f1ab7062ae889ac4b45e1e73a6e3943
Fred Vuni <fredvuni809@gmail.com>
2023-11-18 13:34:04 +0300

    chore: use google auth to signin

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,11 +3,18 @@
 import React from "react";
 import styles from "./loginPage.module.css";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Loginpage = () => {
   const { data, status } = useSession();
-
-  console.log({ data: data, status: status });
+  const router = useRouter();
+  // console.log({ data: data, status: status });
+  if (status === "loading") {
+    return <div className={styles.loading}>loading ...</div>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
