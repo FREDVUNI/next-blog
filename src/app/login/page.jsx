1752f215a631ebb96fd7f9bbb4e8038ff83ce733
Fred Vuni <fredvuni809@gmail.com>
2023-11-18 13:01:55 +0300

    chore: remove extra log

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,11 +7,13 @@ import { signIn, useSession } from "next-auth/react";
 const Loginpage = () => {
   const { data, status } = useSession();
 
-  console.log({"data":data,"status": status});
+  console.log({ data: data, status: status });
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton} onClick={() => signIn("google")}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
