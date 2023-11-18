7ef59271a10cbd49463e3f8c7e1e13b4f03feb3d
Fred Vuni <fredvuni809@gmail.com>
2023-11-18 13:23:19 +0300

    chore: add GET and POST methods

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/utils/auth";
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
