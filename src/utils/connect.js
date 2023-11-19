cdea3120b289df2a0e31f683d001b109c0ee1759
Fred Vuni <fredvuni809@gmail.com>
2023-11-19 11:34:10 +0300

    fix: check  the node env

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
