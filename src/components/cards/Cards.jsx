8e957d7af76eb2989c08e500a6f23f4926ce218d
Fred Vuni <fredvuni809@gmail.com>
2023-12-16 15:20:35 +0300

    chore: use the posts endpoint

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,15 +3,30 @@ import styles from "./cards.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
 
-const Cards = () => {
+const getPosts = async () => {
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/posts`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("No posts found.");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Cards = async () => {
+  const posts = await getPosts();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts &&
+          posts.map((post) => (
+            <Card title={post.title} desc={post.desc} img={post.img} />
+          ))}
       </div>
       <Pagination />
     </div>
