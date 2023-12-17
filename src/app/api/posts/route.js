6136329c0e3fb848bd690bea79aa49319e384981
Fred Vuni <fredvuni809@gmail.com>
2023-12-17 15:22:49 +0300

    feat: fetch posts from posts endpoint

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,16 +1,23 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req) => {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
+
+  const POST_PER_PAGE = 2;
   try {
-    const posts = await prisma.post.findMany();
-    return NextResponse(
+    const posts = await prisma.post.findMany({
+      take: POST_PER_PAGE,
+      skip: page - 1,
+    });
+    return new NextResponse(
       JSON.stringify(posts, {
         status: 200,
       })
     );
   } catch (error) {
-    return NextResponse(
+    return new NextResponse(
       JSON.stringify(
         {
           message: "something went wrong",
