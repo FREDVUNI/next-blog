197f90604a541ef6ea2ace3518762c807fa0cc51
Fred Vuni <fredvuni809@gmail.com>
2023-12-18 09:11:11 +0300

    chore: fix  category route

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    const categories = await prisma.category.findMany();
+    const categories = await prisma.category.findMany({});
     return new NextResponse(JSON.stringify(categories, { status: 200 }));
   } catch (error) {
     console.log(error);
