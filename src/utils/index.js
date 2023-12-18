06bde9d49f50930dfa645e3b5cd78d817cf61b24
Fred Vuni <fredvuni809@gmail.com>
2023-12-18 09:29:36 +0300

    feat: make category upper case

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,10 @@ export const capitalizeWord = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
 
+export const makeUpperCase = (word) => {
+  return word.toUpperCase();
+};
+
 export const shortenDesc = (desc, maxLength) => {
   if (desc.length > maxLength) {
     return desc.substring(0, maxLength - 3) + "...";
