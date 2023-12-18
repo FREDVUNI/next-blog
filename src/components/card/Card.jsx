f114d0aac0418c9ee7655aa2bd97624bb62208b2
Fred Vuni <fredvuni809@gmail.com>
2023-12-18 09:12:36 +0300

    feat: add elipsis to description

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { shortenDesc } from "@/utils";
 
-const Card = ({ key, title, desc, img, category }) => {
+const Card = ({ key, title, desc, img, cat }) => {
   return (
     <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
@@ -12,12 +13,12 @@ const Card = ({ key, title, desc, img, category }) => {
       <div className={styles.textContainer}>
         <div className={styles.details}>
           <span className={styles.date}>11.05.20</span>
-          <span className={styles.category}> - CULTURE</span>
+          <span className={styles.category}> - CULTURE {cat}</span>
         </div>
         <Link href={"/"}>
           <h2>{title}</h2>
         </Link>
-        <p className={styles.desc}>{desc}</p>
+        <p className={styles.desc}>{shortenDesc(desc, 250)}</p>
         <Link href={"/"} className={styles.link}>
           Read more
         </Link>
