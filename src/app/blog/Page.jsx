8d395e3cd08eafe8732e018f4bd742b0411ac791
Fred Vuni <fredvuni809@gmail.com>
2023-12-26 06:05:48 +0300

    chore: refactor posts and category posts fetch

--- src/app/blog/Page.jsx
+++ src/app/blog/Page.jsx
@@ -4,12 +4,15 @@ import styles from "./blogPage.module.css";
 import Cards from "@/components/cards/Cards";
 import Menu from "@/components/menu/Menu";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Style Blog</h1>
       <div className={styles.content}>
-        <Cards />
+        <Cards page={page} cat={cat} />
         <Menu />
       </div>
     </div>
