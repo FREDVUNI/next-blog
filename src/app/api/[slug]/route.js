a7e9714a1a250969ee36b697650afdf0e75f8db6
Fred Vuni <fredvuni809@gmail.com>
2023-12-28 15:11:04 +0300

    feat: get single posts

---
+++ src/app/api/[slug]/route.js
@@ -0,0 +1,29 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  const { slug } = params;
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        slug,
+      },
+      include: {
+        user: true,
+      },
+    });
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify(
+        {
+          message: "something went wrong",
+        },
+        {
+          status: 500,
+        }
+      )
+    );
+  }
+};
