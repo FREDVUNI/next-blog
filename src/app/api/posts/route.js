2e3bb08b4a5604fe687ffbf15c4237ee06b2a943
Fred Vuni <fredvuni809@gmail.com>
2023-12-30 13:40:56 +0300

    chore: create endpoint for creating posts

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -37,6 +38,30 @@ export const GET = async (req) => {
         },
         { status: 500 }
       )
+    );
+  }
+};
+
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "you\'re not authenticated." }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const post = await prisma.post.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong." }, { status: 500 })
     );
   }
 };
