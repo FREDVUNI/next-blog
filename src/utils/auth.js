5a15d0f5c51c3908f864aef423cfe0d178d68300
Fred Vuni <fredvuni809@gmail.com>
2024-03-08 20:54:54 +0300

    chore: remove github provider/service

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,10 +11,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
   ],
 };
 export const getAuthSession = () => getServerSession(authOptions);
